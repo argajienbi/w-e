4e668d6b7176bfb2ae4f609be2dcf089a2fed032
argajienbi <armin.gandi@gmail.com>
2019-01-08 22:00:56 +0700

    Add files via upload

---
+++ we.php
@@ -0,0 +1,23 @@
+<?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$sungging = $putih.'[*] '.$ijo.'subcribe youtube sungging '.$putih.'[-] code: '.$ijo.'L41023';
+$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
+$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging".$t.$red."[°] demi keamanan hentikan aktifitas bot sekarang !!!!".$t.$kuning."[•] ctrl + c";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+$weblock =[""];
+$ydblock = [""];
+$we_stat = true;
+$msgwe = "[!] oops sorry silahkan cek billboard w&e".$t.$red."[•] demi keamanan hentikan aktivitas ini  ".$t.$kuning."[--] ctrl+c kalo ngeyel bodo amat".$t.$turkis."[•] pahami dengan baik jangan  banyak tanya ";
+$msgveeu = "[!] enjoy your life";
+
+$data= [
+'Ana Maryanah', 'Mutia Amelia', 'Armin Gandi', 'Mustofa Ya mustofa', 'Smkn 1 Baradatu', 'alvin hidayat', 'Ilhamm Wacth earn', 'Muhammad Kah', 'Ato Bara bere', 'fara ciprit05', 'Gudang Anime Channel', 'Doni W&M",
+
+];
+
+$toto = ['tes',];
+$ydnews = ['tes','adidoank',];
